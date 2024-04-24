86b4bd79ba599c529e6275e1d9fa1ca7471018e1
vutung172 <vuthanhtung1721991@gmail.com>
2024-04-24 16:34:26 +0900

    commit SQL script

--- Ecommerce/src/main/java/com/main/ra/SQLScript/DDL/DDL.sql
+++ Ecommerce/src/main/java/com/main/ra/SQLScript/DDL/DDL.sql
@@ -55,7 +55,7 @@ CREATE TABLE user_roles(
     role_id bigint NOT NULL COMMENT '許可ID',
     PRIMARY KEY (user_id, role_id)
 );
-# user_role foreign key
+# user_role FOREIGN KEY
 ALTER TABLE user_roles
     ADD CONSTRAINT fk_user_roles_role_id
         FOREIGN KEY (role_id) REFERENCES roles (role_id);
@@ -72,7 +72,7 @@ CREATE TABLE categories(
 );
 
 # products table
-create table products
+CREATE TABLE products
 (
     product_id     bigint PRIMARY KEY AUTO_INCREMENT COMMENT '商品ID',
     sku            varchar(100)                      COMMENT '商品コード',
@@ -111,7 +111,7 @@ END;
 // DELIMITER ;
 
 #orders table
-create table orders(
+CREATE TABLE orders(
     order_id        bigint PRIMARY KEY AUTO_INCREMENT                                      COMMENT '注文ID',
     serial_number   varchar(100)                                                           COMMENT 'シリアル番号',
     user_id         bigint                                                        NOT NULL COMMENT 'ユーザーID',
@@ -124,8 +124,98 @@ create table orders(
     created_at      date                                                                   COMMENT '作成日付',
     received_at     date                                                                   COMMENT '配達予定日'
 );
-# orders foreign key
+# orders FOREIGN KEY
 ALTER TABLE orders
     ADD CONSTRAINT fk_orders_user_id
-        FOREIGN KEY (user_id) REFERENCES users (user_id)
+        FOREIGN KEY (user_id) REFERENCES users (user_id);
 
+# order triggers
+DELIMITER //
+CREATE TRIGGER tgr_created_date_order_datestamp
+    BEFORE INSERT ON orders FOR EACH ROW
+BEGIN
+    SET NEW.created_at = CURRENT_DATE;
+END;
+// DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER tgr_updated_date_product_datestamp
+    BEFORE INSERT ON orders FOR EACH ROW
+BEGIN
+    SET NEW.received_at = DATE_ADD(CURRENT_DATE, INTERVAL 4 DAY);
+END;
+// DELIMITER ;
+
+# order_details table
+CREATE TABLE order_details(
+    order_id       bigint         NOT NULL  COMMENT '注文ID',
+    product_id     bigint         NOT NULL  COMMENT '商品ID',
+    name           varchar(100)             COMMENT '商品名前',
+    unit_price     decimal(10, 2)           COMMENT 'ユニット価格',
+    order_quantity int                      COMMENT '注文数量',
+    PRIMARY KEY (order_id, product_id)
+);
+# order_details FOREIGN KEY
+ALTER TABLE order_details
+    ADD CONSTRAINT fk_orders_details_oder_id
+        FOREIGN KEY (order_id) REFERENCES orders (order_id);
+ALTER TABLE order_details
+    ADD CONSTRAINT fk_orders_details_product_id
+        FOREIGN KEY (product_id) REFERENCES products (product_id);
+# order_details checker
+ALTER TABLE order_details
+    ADD CONSTRAINT chk_order_qty_order_details
+        CHECK (order_quantity > 0);
+
+# shopping carts
+CREATE TABLE shopping_carts(
+    shopping_cart_id bigint PRIMARY KEY AUTO_INCREMENT COMMENT 'ショッピングカート',
+    product_id       bigint                             COMMENT '商品ID',
+    user_id          bigint                             COMMENT 'ユーザーID',
+    order_quantity   int                                COMMENT '注文数量',
+    constraint fk_cart_product_id
+        FOREIGN KEY (product_id) REFERENCES products (product_id),
+    constraint fk_cart_user_id
+        FOREIGN KEY (user_id) REFERENCES users (user_id),
+    constraint chk_order_qty_carts
+        check (`order_quantity` > 0)
+);
+# shopping carts FOREIGN KEY
+ALTER TABLE shopping_carts
+    ADD CONSTRAINT fk_cart_product_id
+        FOREIGN KEY (product_id) REFERENCES products (product_id);
+ALTER TABLE shopping_carts
+    ADD CONSTRAINT fk_cart_user_id
+        FOREIGN KEY (user_id) REFERENCES users (user_id);
+# shopping carts checker
+ALTER TABLE shopping_carts
+    ADD CONSTRAINT chk_order_qty_carts
+        CHECK (order_quantity > 0);
+
+# address table
+CREATE TABLE address(
+    address_id   bigint PRIMARY KEY AUTO_INCREMENT COMMENT '住所ID',
+    user_id      bigint       COMMENT 'ユーザーID',
+    full_address varchar(255) COMMENT '住所詳細',
+    phone        varchar(15)  COMMENT '電話番号',
+    receive_name varchar(50)  COMMENT '届け先の名前'
+);
+# address foreign key
+ALTER TABLE address
+    ADD CONSTRAINT fk_address_user_id
+        FOREIGN KEY (user_id) REFERENCES users (user_id);
+
+# Wish_lists table
+CREATE TABLE wish_lists
+(
+    wish_list_id bigint PRIMARY KEY AUTO_INCREMENT COMMENT 'ウィッシュリスト',
+    user_id      bigint                            COMMENT 'ユーザーID',
+    product_id   bigint                            COMMENT '商品ID'
+);
+# wish_lists
+ALTER TABLE wish_lists
+    ADD CONSTRAINT fk_wish_lists_product_id
+        FOREIGN KEY (product_id) REFERENCES products (product_id);
+ALTER TABLE wish_lists
+    ADD CONSTRAINT fk_wish_lists_user_id
+        FOREIGN KEY (user_id) REFERENCES users (user_id);
